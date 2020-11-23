9ea771b6b44a3feba41fa921645e8bca561d2b3c
Kilian Smiti <kilian@smiti.fr>
2020-11-24 00:25:03 +0100

    fix: remove quote for auth and middleware

--- parser/api.go
+++ parser/api.go
@@ -121,8 +121,8 @@ func (a *API) _route(data string) error {
 func (a *API) _auth(data string) error {
 	rgx := regexp.MustCompile(`(?m)(?:"|')(.*?)(?:"|')`)
 	for _, role := range rgx.FindAllString(data, -1) {
-		role = strings.Trim("\"", "")
-		role = strings.Trim("'", "")
+		role = strings.Trim(role, "\"")
+		role = strings.Trim(role, "'")
 		a.Authorization = append(a.Authorization, role)
 	}
 	return nil
@@ -131,8 +131,8 @@ func (a *API) _auth(data string) error {
 func (a *API) _middleware(data string, before bool) error {
 	rgx := regexp.MustCompile(`(?m)(?:"|')(.*?)(?:"|')`)
 	for _, mw := range rgx.FindAllString(data, -1) {
-		mw = strings.Trim("\"", "")
-		mw = strings.Trim("'", "")
+		mw = strings.Trim(mw, "\"")
+		mw = strings.Trim(mw, "'")
 		if before {
 			a.Middleware.Before = append(a.Middleware.Before, mw)
 		} else {
